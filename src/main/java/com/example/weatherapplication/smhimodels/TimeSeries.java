a21c130945e2b4add3cb5fc423f907b4c0138f8d
anthonyk7 <Gungstol02>
2022-05-05 13:43:42 +0200

    initial commit

---
+++ src/main/java/com/example/weatherapplication/smhimodels/TimeSeries.java
@@ -0,0 +1,62 @@
+
+package com.example.weatherapplication.smhimodels;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+
+import com.example.Parameter;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "validTime",
+    "parameters"
+})
+@Generated("jsonschema2pojo")
+public class TimeSeries {
+
+    @JsonProperty("validTime")
+    private String validTime;
+    @JsonProperty("parameters")
+    private List<Parameter> parameters = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("validTime")
+    public String getValidTime() {
+        return validTime;
+    }
+
+    @JsonProperty("validTime")
+    public void setValidTime(String validTime) {
+        this.validTime = validTime;
+    }
+
+    @JsonProperty("parameters")
+    public List<Parameter> getParameters() {
+        return parameters;
+    }
+
+    @JsonProperty("parameters")
+    public void setParameters(List<Parameter> parameters) {
+        this.parameters = parameters;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
